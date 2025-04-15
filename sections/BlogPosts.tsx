13e3e59ba66c8b526b1d7a10e0cd8471e606da0a
ryansantos <ryan.ribeiro@cadastra.com>
2025-04-15 09:43:38 -0300

    fix: ajustes

--- sections/BlogPosts.tsx
+++ sections/BlogPosts.tsx
@@ -121,7 +121,7 @@ export default function ProjectsShowcase({
           {projects?.map((project, index) => (
             <div key={index} class="bg-white rounded-xl overflow-hidden shadow-lg">
               {/* Cabeçalho do projeto */}
-              <div class="bg-gradient-to-r from-green-700 to-green-900 px-8 py-2 text-white flex flex-row gap-4 justify-between items-center h-40 max-h-40">
+              <div class="bg-gradient-to-r from-green-700 to-green-900 pl-8 pr-2 py-2 text-white flex flex-row gap-4 justify-between items-center h-40 max-h-40">
                 <div class="flex flex-col gap-4">
                   <h3 class="text-3xl font-bold mb-2">{project.title}</h3>
                   <p class="text-xl font-medium text-green-100">{project.subtitle}</p>
@@ -129,7 +129,7 @@ export default function ProjectsShowcase({
                 <Image
                   width={project.logo?.width}
                   height={project.logo?.height}
-                  class="max-w-24 w-auto max-h-20 h-auto object-cover" 
+                  class="w-auto h-full object-cover rounded" 
                   src={project.logo?.src}
                   alt={project.logo?.alt}
                 />
